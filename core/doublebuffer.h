7f3b38b4d2efbdd1621f0ae003bc520064c73985
Reche Maynard <reche.maynard95@gmail.com>
2026-01-21 19:17:02 +0000

    Added a plans.txt to help visualise my nexxt steps.

--- core/doublebuffer.h
+++ core/doublebuffer.h
@@ -1,5 +1,6 @@
 #ifndef DOUBLEBUFFER_H
 #define DOUBLEBUFFER_H
+#include <algorithm>
 #include <array>
 #include <atomic>
 #include <cstddef>
@@ -8,8 +9,6 @@ namespace Core {
 
 template<typename T>
 class DoubleBuffer {
-
-public:
 private:
     // Both the read and write buffers.
     std::array<T*, 2> buffers;
@@ -25,6 +24,82 @@ private:
     std::atomic<size_t> readPos{0};
     // The position within the buffer to write to in the engine thread.
     std::atomic<size_t> writePos{0};
+
+public:
+
+    // Write buffer logic for use in the audio engine thread.
+
+    // Get the write buffer to be filled.
+    T* getWriteBuffer() noexcept {
+        return buffers[writeIndex.load(std::memory_order_acquire)];
+
+    }
+
+    // get the write position for stream writes.
+    size_t getWritePosition() const noexcept {
+        return writePos.load(std::memory_order_relaxed);
+    }
+
+    // write a sample to the write buffer (for streamig mode.)
+    void writeSample(T sample) noexcept {
+        size_t posSnapshot = writePos.load(std::memory_order_relaxed);
+        buffers[writeIndex.load(std::memory_order_acquire)][posSnapshot] = sample;
+        writePos.store(posSnapshot + 1, std::memory_order_release);
+    }
+
+    // Write a block to the write buffer
+    void writeBlock(const T* data, size_t count) noexcept {
+        int idx = writeIndex.load(std::memory_order_acquire);
+        size_t pos = writePos.load(std::memory_order_relaxed);
+        std::copy_n(data, count, buffers[idx] + pos);
+        writePos.store(pos + count, std::memory_order_release);
+    }
+
+    // Commit Write
+    // TODO: Implement commitWrite functionality.
+
+
+
+
+    // Read buffer logic for use in the audio playback thread.
+
+
+    // Return the read buffer.
+    T* getReadBuffer() noexcept {
+        return buffers[readIndex.load(std::memory_order_acquire)];
+    }
+
+    // Get the current index of the read buffer for sample based reading.
+    size_t getReadPosition() const noexcept {
+        return readPos.load(std::memory_order_relaxed);
+
+    }
+    // Returns the current sample for the read buffer, swaps buffer if the read position reaches capacity.
+    T readSample() noexcept {
+        size_t posSnapshot = readPos.load(std::memory_order_relaxed);
+        T sample = buffers[readIndex.load(std::memory_order_acquire)];
+        readPos.store(posSnapshot + 1, std::memory_order_release);
+
+        //TODO:: Implement swap buffers helper function.
+
+        if(readPos >= m_capacity) {
+
+            //swapBuffers();
+        }
+        return sample;
+    }
+
+    void readBlock(T* output, size_t count) noexcept {
+        int idx = readIndex.load(std::memory_order_acquire);
+        size_t pos = readPos.load(std::memory_order_relaxed);
+        std::copy_n(buffers[idx] + pos, count, output);
+
+        readPos.store(pos + count, std::memory_order_release);
+        if(readPos >= m_capacity) {
+
+            //swapBuffers();
+        }
+    }
 };
 }; // namespace Core
 #endif // DOUBLEBUFFER_H
---
+++ plans.md
@@ -0,0 +1,83 @@
+<html>
+<head>
+<style>
+* {
+box-sizing:border-box;
+}
+body {
+padding:0;
+margin:0;
+}
+h1 {
+    font-weight:200; 
+    padding:0;
+
+}
+
+</style>
+</head>
+<body>
+<div class="headerWrapper">
+<h1>Plans</h1>
+</div>
+<h3>Track Double Buffer</h3>
+
+<p>
+The double buffer base class needs to be abstracted into AudioDoubleBuffer.
+</p>
+<p>
+This is for track level processing and will allow all tracks to 
+implement a lock free way to to write to their buffer ahead of playback time.
+In a seperate thread.
+</p>
+<p>
+It will be a double buffer of type float, and will need some further
+functionality.
+</p>
+<p>
+For now this will just mean the ability to process an entire block
+with added functionality, 
+however in the future it will enable us to process lookaheads.
+</p>
+<hr/>
+<h3>Audio Engine Read Thread</h3>
+<p> A dedicated thread that allows me to implement the use of my 
+double buffers. rather than hacking the mixMasterbuffer into the 
+gui thread where it is currently.
+</p>
+<p>
+This will further open the doors and allow me to accurately parse midi events
+and generate audio buffers at a track level, with much less concern regarding read
+and write indexes as it will implement checking functionality for the available samples.
+</p>
+<p>
+Double buffers will also be magnitudes larger than the projects buffersize. 
+This will allow complex computation that may have previously caused a 
+stuttering effect.
+</p>
+<hr/>
+<h3>Global Project Time</h3>
+<p>A single source of truth for the samplerate, bpm, tpq, beats per bar, 
+counts per beat, etc. All tracks, clips, assets and effects can have a reference to this
+so that when any of the values are changed there is no need to call update on every device.
+</p>
+<p>
+None of the values should be automatable. and the change of any value would require a stop of audio. 
+
+If we want to implement bpm or time signature automations at a later point. 
+
+We will need to implement a tempo map, and a time signature map. 
+The tempo map can include int64_t end tick and start tick.
+as well as double start bpm end bpm. This allows the ability for
+automation of the bpm without editing our single source of truth,
+which would interfere with our processing logic. (samples to ticks to seconds etc).
+</p>
+
+<p>
+If we want a time signature map the same would apply however time signature is not a continuos value.
+so we can just store a single snapshot including the int beats per bar and bars per beat.
+</p>
+
+<p></p>
+</body>
+</html>
